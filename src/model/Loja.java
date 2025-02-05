58669ebf4591b781b979efd3d047ad27437803ff
Louzadino <guilhermelouzada852@gmail.com>
2025-02-05 17:41:00 -0300

    Classe Loja - primeira implementação

--- src/model/Loja.java
+++ src/model/Loja.java
@@ -2,10 +2,34 @@ package model;
 
 /**
  * Classe que representa uma loja no sistema.
+ * A loja é sempre uma pessoa jurídica.
  */
 public class Loja extends PessoaJuridica {
 
-    public Loja (String nome, String telefone, Endereco endereco, String cnpj) {
+    /**
+     * Construtor da classe Loja.
+     *
+     * @param nome      Nome da loja.
+     * @param telefone  Telefone de contato da loja.
+     * @param endereco  Endereço da loja.
+     * @param cnpj      CNPJ da loja (14 dígitos numéricos).
+     */
+    public Loja(String nome, String telefone, Endereco endereco, String cnpj) {
         super(nome, telefone, endereco, cnpj);
+    }
+
+    /**
+     * Retorna uma representação textual da loja.
+     *
+     * @return String formatada com os dados da loja.
+     */
+    @Override
+    public String toString() {
+        return "Loja{" +
+               "nome='" + getNome() + '\'' +
+               ", telefone='" + getTelefone() + '\'' +
+               ", cnpj='" + getCnpj() + '\'' +
+               ", endereco='" + getEndereco() + '\'' +
+               '}';
     }
 }
